b3160c609eba664dbcf56415ec0a47d75a2c010a
rizalyoga <ach.rizal.yogaswara@gmail.com>
2025-04-29 09:52:00 +0700

    feat: create loading skeleton

--- src/components/pages/Dashboard/Index.tsx
+++ src/components/pages/Dashboard/Index.tsx
@@ -21,23 +21,36 @@ const DashboardIndex = () => {
 
   if (isLoading) {
     return (
-      <Card>
-        <CardContent className="p-6">
-          <div className="text-center">Loading...</div>
-        </CardContent>
-      </Card>
+      <DashboardLayout>
+        <Card>
+          <CardContent className="p-6 min-h-dvh">
+            <div className="mt-28 max-w-7xl border border-slate-400 h-64 rounded-xl mx-auto px-4 sm:px-6 lg:px-8 bg-slate-200 shadow-2xl mb-2 animate-pulse flex justify-center flex-col gap-2 py-6">
+              <p className="text-black text-start font-bold mb-5">
+                Job Applications
+              </p>
+              <div className="w-full bg-slate-400 border rounded-md h-10"></div>
+              <div className="w-full bg-slate-400 border rounded-md h-10"></div>
+              <div className="w-full bg-slate-400 border rounded-md h-10"></div>
+              <div className="w-full bg-slate-400 border rounded-md h-10"></div>
+              <div className="w-full bg-slate-400 border rounded-md h-10"></div>
+            </div>
+          </CardContent>
+        </Card>
+      </DashboardLayout>
     );
   }
 
   if (error) {
     return (
-      <Card>
-        <CardContent className="p-6">
-          <div className="text-center text-red-500">
-            Error: {(error as Error).message}
-          </div>
-        </CardContent>
-      </Card>
+      <DashboardLayout>
+        <Card>
+          <CardContent className="p-6">
+            <div className="text-center text-red-500">
+              Error: {(error as Error).message}
+            </div>
+          </CardContent>
+        </Card>
+      </DashboardLayout>
     );
   }
 
